a63a74637323ead4ec649dab377f90c0b1e4e733
David Monaghan <monaghan.david@gmail.com>
2013-10-28 00:33:57 -0400

    init

--- server.js
+++ server.js
@@ -78,17 +78,6 @@ function broadcast(event, data) {
   });
 }
 
-
-function domainrLookup(name) {
-    return '';
-}
-function twitterNameTaken(name) {
-    return true;
-}
-function facebookNameTaken(name) {
-    return true;
-}
-
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
